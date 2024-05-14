e39d8c097d8a229e85a1a7f12f789689fbbf0e5e
ilerJo <104171180+ilerJo@users.noreply.github.com>
2024-05-13 17:54:59 -0700

    Update Popper_Individual_1.py
    Updated the Comments

--- Popper_Individual_1.py
+++ Popper_Individual_1.py
@@ -8,20 +8,20 @@ from src.Command import Command
 from src.State import BehaviorState, State
 from MangDang.mini_pupper.display import Display
 
+# Main Loop
 def main():
-    config = Configuration()
+    config = Configuration()    
     hardware_interface = HardwareInterface()
     disp = Display()
-    controller = Controller(config, four_legs_inverse_kinematics)
+    controller = Controller(config, four_legs_inverse_kinematics)    # Initializes Controller with Kinematics
     state = State()
-    state.quat_orientation = np.array([1, 0, 0, 0])
+    state.quat_orientation = np.array([1, 0, 0, 0])    # Initial State of Oreientation, use quatenion method
     command = Command()
 
     disp.show_ip()  # Display the IP address on the robot's display
 
-    # Define the sequence of movements
-    movements = ["forward", "circle_left", "circle_right", "jump"]
-    current_movement_index = 0
+    movements = ["forward", "circle_left", "circle_right", "jump"]    # Defining the Movement states
+    current_movement_index = 0    # Movement Count
 
     last_loop = time.time()
     while True:
@@ -30,7 +30,7 @@ def main():
             continue
 
         if current_movement_index >= len(movements):
-            current_movement_index = 0  # Restart the sequence
+            current_movement_index = 0  # Restart the loop
 
         current_movement = movements[current_movement_index]
 
@@ -46,15 +46,16 @@ def main():
             command.yaw_rate = -0.5  # Turn right
             state.behavior_state = BehaviorState.TROT
         elif current_movement == "jump":
-            state.behavior_state = BehaviorState.JUMP  # Set the state to jump
+            state.behavior_state = BehaviorState.JUMP  # Set Jump state 
 
         controller.run(state, command, disp)  # Process the current command
         hardware_interface.set_actuator_postions(state.joint_angles)  # Actuate the servos based on computed joint angles
 
-        time.sleep(1)  # Pause for effect, can be adjusted as needed
-        current_movement_index += 1  # Move to the next action in the sequence
+        time.sleep(1)  # Pause for 1 sec
+        current_movement_index += 1  # Move to the next action in loop
 
-        last_loop = time.time()
-
+        last_loop = time.time()    # Timestamp 
+        
+# Main Loop Call
 if __name__ == "__main__":
     main()
