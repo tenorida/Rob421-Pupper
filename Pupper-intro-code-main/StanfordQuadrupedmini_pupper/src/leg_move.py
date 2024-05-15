8430f88bafbf9bba8a5c810aebff069c7d046b2d
NoIdeaWhatIamDoing6 <166052555+NoIdeaWhatIamDoing6@users.noreply.github.com>
2024-05-15 13:15:20 -0700

    Update leg_move.py

--- Pupper-intro-code-main/StanfordQuadrupedmini_pupper/src/leg_move.py
+++ Pupper-intro-code-main/StanfordQuadrupedmini_pupper/src/leg_move.py
@@ -38,9 +38,9 @@ def move_servo15():
 
     os.system("echo" + str(degree_finder) + "> /sys/class/pwm/pwmchip0/pwm11/duty_cycle")
     time.sleep(1)
-    os.system("echo 2500000 > /sys/class/pwm/pwmchip0/pwm14/duty_cycle")
+    os.system("echo 2500000 > /sys/class/pwm/pwmchip0/pwm13/duty_cycle")
     time.sleep(1)
-    os.system("echo 500000 > /sys/class/pwm/pwmchip0/pwm14/duty_cycle")
+    os.system("echo 500000 > /sys/class/pwm/pwmchip0/pwm13/duty_cycle")
 
     print("done")
 
