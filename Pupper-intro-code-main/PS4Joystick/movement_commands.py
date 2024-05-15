c29cf287b9a10ab96c5769df8918955e98ef5496
tenorida <146912063+tenorida@users.noreply.github.com>
2024-05-15 14:35:45 -0700

    u

--- Pupper-intro-code-main/PS4Joystick/movement_commands.py
+++ Pupper-intro-code-main/PS4Joystick/movement_commands.py
@@ -126,7 +126,7 @@ if __name__ == "__main__":
     print("moving foward")
     for i in range(0,7):
         move_forward(0.8)
-        time.sleep(1)
+        #time.sleep(1)
         #print("adjusting")
         #move_left()
         #time.sleep(5)
