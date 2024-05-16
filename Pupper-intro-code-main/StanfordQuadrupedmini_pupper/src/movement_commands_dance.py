5140b94de80bfc8fa74642fe50b32dbc3d5b98d7
NoIdeaWhatIamDoing6 <166052555+NoIdeaWhatIamDoing6@users.noreply.github.com>
2024-05-16 15:45:25 -0700

    Create movement_commands_dance.py

---
+++ Pupper-intro-code-main/StanfordQuadrupedmini_pupper/src/movement_commands_dance.py
@@ -0,0 +1,138 @@
+from UDPComms import Publisher
+import time
+
+drive_pub = Publisher(8830)
+
+def ActDeactivate():
+    drive_pub.send({"L1": 1, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+
+def trot():
+    drive_pub.send({"L1": 0, 
+            "R1": 1, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+
+def stop():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+
+def move_forward(x):
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": x, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+    
+def move_left():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": -1, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+
+def move_right():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 1, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+
+def move_backwards():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": -1, 
+            "lx": 1, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+
+if __name__ == "__main__":
+    ActDeactivate()
+    time.sleep(1)
+    trot()
+    print("start trot")
+    time.sleep(1)
+    t0 = time.time()
+    diff = (time.time() - t0)
+    print (t0 * pow(10,3))
+    print(diff* pow(10,3))
+    print("moving foward")
+    while diff < 10000:
+        move_forward(0.4)
+        time.sleep(0.1)
+        #print("adjusting")
+        #move_left()
+        #time.sleep(5)
+        diff = (time.time() - t0) * pow(10,3) # msec
+        #print(diff)
+    ActDeactivate()
+    print("done")
