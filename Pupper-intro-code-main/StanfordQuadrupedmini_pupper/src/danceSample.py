7bb5405632cd87277a1019a13373e908c0640087
NoIdeaWhatIamDoing6 <166052555+NoIdeaWhatIamDoing6@users.noreply.github.com>
2024-05-16 15:54:31 -0700

    Update danceSample.py

--- Pupper-intro-code-main/StanfordQuadrupedmini_pupper/src/danceSample.py
+++ Pupper-intro-code-main/StanfordQuadrupedmini_pupper/src/danceSample.py
@@ -44,7 +44,7 @@ from MovementGroup import MovementGroups
 from movement_commands_dance import trot, ActDeactivate
 Move = MovementGroups()
 
-ActDeactive()
+ActDeactivate()
 time.sleep(0.2)
 trot()
 time.sleep(0.2)
