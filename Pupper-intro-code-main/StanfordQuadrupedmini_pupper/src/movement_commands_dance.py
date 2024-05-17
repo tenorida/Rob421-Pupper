f4217a80607929ac2df35ac6601c6994978a7bdd
NoIdeaWhatIamDoing6 <166052555+NoIdeaWhatIamDoing6@users.noreply.github.com>
2024-05-17 15:22:27 -0700

    Update movement_commands_dance.py

--- Pupper-intro-code-main/StanfordQuadrupedmini_pupper/src/movement_commands_dance.py
+++ Pupper-intro-code-main/StanfordQuadrupedmini_pupper/src/movement_commands_dance.py
@@ -2,118 +2,118 @@ from UDPComms import Publisher
 import time
 
 drive_pub = Publisher(8830)
-
-def ActDeactivate():
-    drive_pub.send({"L1": 1, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": 0, 
-            "rx": 0, 
-            "message_rate": 20, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})
-
-def trot():
-    drive_pub.send({"L1": 0, 
-            "R1": 1, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": 0, 
-            "rx": 0, 
-            "message_rate": 20, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})
-
-def stop():
-    drive_pub.send({"L1": 0, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": 0, 
-            "rx": 0, 
-            "message_rate": 20, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})
-
-def move_forward(x):
-    drive_pub.send({"L1": 0, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": x, 
-            "lx": 0, 
-            "rx": 0, 
-            "message_rate": 20, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})
+class DanceMoveCommands:
+    def ActDeactivate():
+        drive_pub.send({"L1": 1, 
+                "R1": 0, 
+                "x": 0, 
+                "circle": 0, 
+                "triangle": 0, 
+                "L2": 0, 
+                "R2": 0, 
+                "ly": 0, 
+                "lx": 0, 
+                "rx": 0, 
+                "message_rate": 20, 
+                "ry": 0, 
+                "dpady": 0, 
+                "dpadx": 0})
     
-def move_left():
-    drive_pub.send({"L1": 0, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": -1, 
-            "rx": 0, 
-            "message_rate": 20, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})
-
-def move_right():
-    drive_pub.send({"L1": 0, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": 0, 
-            "lx": 1, 
-            "rx": 0, 
-            "message_rate": 20, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})
-
-def move_backwards():
-    drive_pub.send({"L1": 0, 
-            "R1": 0, 
-            "x": 0, 
-            "circle": 0, 
-            "triangle": 0, 
-            "L2": 0, 
-            "R2": 0, 
-            "ly": -1, 
-            "lx": 1, 
-            "rx": 0, 
-            "message_rate": 20, 
-            "ry": 0, 
-            "dpady": 0, 
-            "dpadx": 0})
+    def trot():
+        drive_pub.send({"L1": 0, 
+                "R1": 1, 
+                "x": 0, 
+                "circle": 0, 
+                "triangle": 0, 
+                "L2": 0, 
+                "R2": 0, 
+                "ly": 0, 
+                "lx": 0, 
+                "rx": 0, 
+                "message_rate": 20, 
+                "ry": 0, 
+                "dpady": 0, 
+                "dpadx": 0})
+    
+    def stop():
+        drive_pub.send({"L1": 0, 
+                "R1": 0, 
+                "x": 0, 
+                "circle": 0, 
+                "triangle": 0, 
+                "L2": 0, 
+                "R2": 0, 
+                "ly": 0, 
+                "lx": 0, 
+                "rx": 0, 
+                "message_rate": 20, 
+                "ry": 0, 
+                "dpady": 0, 
+                "dpadx": 0})
+    
+    def move_forward(x):
+        drive_pub.send({"L1": 0, 
+                "R1": 0, 
+                "x": 0, 
+                "circle": 0, 
+                "triangle": 0, 
+                "L2": 0, 
+                "R2": 0, 
+                "ly": x, 
+                "lx": 0, 
+                "rx": 0, 
+                "message_rate": 20, 
+                "ry": 0, 
+                "dpady": 0, 
+                "dpadx": 0})
+        
+    def move_left():
+        drive_pub.send({"L1": 0, 
+                "R1": 0, 
+                "x": 0, 
+                "circle": 0, 
+                "triangle": 0, 
+                "L2": 0, 
+                "R2": 0, 
+                "ly": 0, 
+                "lx": -1, 
+                "rx": 0, 
+                "message_rate": 20, 
+                "ry": 0, 
+                "dpady": 0, 
+                "dpadx": 0})
+    
+    def move_right():
+        drive_pub.send({"L1": 0, 
+                "R1": 0, 
+                "x": 0, 
+                "circle": 0, 
+                "triangle": 0, 
+                "L2": 0, 
+                "R2": 0, 
+                "ly": 0, 
+                "lx": 1, 
+                "rx": 0, 
+                "message_rate": 20, 
+                "ry": 0, 
+                "dpady": 0, 
+                "dpadx": 0})
+    
+    def move_backwards():
+        drive_pub.send({"L1": 0, 
+                "R1": 0, 
+                "x": 0, 
+                "circle": 0, 
+                "triangle": 0, 
+                "L2": 0, 
+                "R2": 0, 
+                "ly": -1, 
+                "lx": 1, 
+                "rx": 0, 
+                "message_rate": 20, 
+                "ry": 0, 
+                "dpady": 0, 
+                "dpadx": 0})
 
 if __name__ == "__main__":
     ActDeactivate()
