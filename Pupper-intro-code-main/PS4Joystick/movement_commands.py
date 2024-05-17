cd5f6112acb098637f51cdd3e9e960faa02180df
tenorida <146912063+tenorida@users.noreply.github.com>
2024-05-17 16:35:42 -0700

    s

--- Pupper-intro-code-main/PS4Joystick/movement_commands.py
+++ Pupper-intro-code-main/PS4Joystick/movement_commands.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
     print (t0 * pow(10,3))
     print(diff* pow(10,3))
     print("moving foward")
-    while diff < 10000:
+    while diff < 7000:
         move_forward(0.4)
         time.sleep(0.1)
         pitch(-0.001)
