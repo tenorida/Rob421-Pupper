fe11936144d38e64494726546e5b59d0ddd8c05d
tenorida <146912063+tenorida@users.noreply.github.com>
2024-05-20 14:43:16 -0700

    s

--- Pupper-intro-code-main/PS4Joystick/movement_commands.py
+++ Pupper-intro-code-main/PS4Joystick/movement_commands.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
         #time.sleep(0.1)
         #print("adjusting")
         #move_left()
-        if (i/8)== i//8:
+        if (i/15)== i//15:
         #pitch(-0.1)
         #time.sleep(3)
         #pitch(0.1)
