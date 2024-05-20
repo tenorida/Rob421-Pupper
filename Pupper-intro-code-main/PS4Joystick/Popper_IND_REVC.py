12c80f1de06c8b30aee55b98664c3e61691282d7
tenorida <146912063+tenorida@users.noreply.github.com>
2024-05-20 13:30:33 -0700

    s

--- Pupper-intro-code-main/PS4Joystick/Popper_IND_REVC.py
+++ Pupper-intro-code-main/PS4Joystick/Popper_IND_REVC.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
     controller.trot()
     time.sleep(1)
 
-    controller.run_for_duration(controller.move_forward, 2, 0.4)    # Forward Control
+    #controller.run_for_duration(controller.move_forward, 2, 0.4)    # Forward Control
     # # controller.run_for_duration(controller.pitch, 2, 0.2)    # Pitch Control
 
     # # controller.stop()
