4c9e6bf9eef90476b4cbffe4d0224a6a8b1e4fa3
saultztristan <165944104+saultztristan@users.noreply.github.com>
2024-05-20 13:02:39 -0700

    Add files via upload

---
+++ Pupper-intro-code-main/PS4Joystick/Open cv attempt code.py
@@ -0,0 +1,23 @@
+#Python Pupper Saultz_Tristan Individual Artifact#
+import cv2
+import time
+
+
+# Open the default camera 
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('Webcam', frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
