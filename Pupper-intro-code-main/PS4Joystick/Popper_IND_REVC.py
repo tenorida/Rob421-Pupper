41f1cfb39c92c9d8ccec7106f4193fefaaaefb39
ilerJo <104171180+ilerJo@users.noreply.github.com>
2024-05-20 11:42:41 -0700

    Update Popper_IND_REVC.py

--- Pupper-intro-code-main/PS4Joystick/Popper_IND_REVC.py
+++ Pupper-intro-code-main/PS4Joystick/Popper_IND_REVC.py
@@ -90,16 +90,16 @@ if __name__ == "__main__":
     controller.run_for_duration(controller.move_left, 5)
     controller.run_for_duration(controller.pitch, 2, 0.2)    # Pitch Control
 
-    controller.trot()
-    time.sleep(1)
+    # controller.trot()
+    # time.sleep(1)
 
     controller.run_for_duration(controller.move_right, 10)
     controller.run_for_duration(controller.pitch, 2, 0.2)    # Pitch Control    
 
     # controller.stop()
     # time.sleep(2)
-    controller.trot()
-    time.sleep(1)
+    # controller.trot()
+    # time.sleep(1)
 
     controller.run_for_duration(controller.move_backwards, 5, -0.2)
     controller.run_for_duration(controller.pitch, 2, 0.2)    # Pitch Control
