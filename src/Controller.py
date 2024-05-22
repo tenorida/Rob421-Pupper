360656088555ab8303867fd9da8120a42b963eb7
NoIdeaWhatIamDoing6 <166052555+NoIdeaWhatIamDoing6@users.noreply.github.com>
2024-05-22 12:49:44 -0700

    Update Controller.py

--- src/Controller.py
+++ src/Controller.py
@@ -1,8 +1,8 @@
-from src.Gaits import GaitController
-from src.StanceController import StanceController
-from src.SwingLegController import SwingController
-from src.Utilities import clipped_first_order_filter
-from src.State import BehaviorState, State
+from Gaits import GaitController
+from StanceController import StanceController
+from SwingLegController import SwingController
+from Utilities import clipped_first_order_filter
+from State import BehaviorState, State
 
 import numpy as np
 from transforms3d.euler import euler2mat, quat2euler
