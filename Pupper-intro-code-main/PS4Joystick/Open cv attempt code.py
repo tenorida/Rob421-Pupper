cc3cca7f30dd62a7a670c94cb2caa52b1f0a7a97
saultztristan <165944104+saultztristan@users.noreply.github.com>
2024-05-23 11:31:58 -0700

    Update Open cv attempt code.py

--- Pupper-intro-code-main/PS4Joystick/Open cv attempt code.py
+++ Pupper-intro-code-main/PS4Joystick/Open cv attempt code.py
@@ -1,23 +1,23 @@
 #Python Pupper Saultz_Tristan Individual Artifact#
-import cv2 
+import cv2 as cv
 import time
 
 
 # Open the default camera 
-cap = cv2.VideoCapture(0, cv2.CAP_V4L)
+cap = cv.VideoCapture(0, cv.CAP_V4L)
 
 while cap.isOpened():
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Display the resulting frame
-    cv2.imshow('Webcam', frame)
+    cv.imshow('Webcam', frame)
 
     # Break the loop if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
 
