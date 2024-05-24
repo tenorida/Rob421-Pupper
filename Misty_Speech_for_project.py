89bac3b395a992df4ed3f5a938be64cab839cad2
NoIdeaWhatIamDoing6 <166052555+NoIdeaWhatIamDoing6@users.noreply.github.com>
2024-05-24 14:27:48 -0700

    Update Misty_Speech_for_project.py

--- Misty_Speech_for_project.py
+++ Misty_Speech_for_project.py
@@ -11,3 +11,16 @@ model = whisper.load_model("base")
 
 ipAddress = "192.168.0.XXX"
 misty = Robot(ipAddress)
+
+
+
+def Key_Phrase_Recognized(data): #Wake up misty with key phrase
+    print(data)
+    misty.DisplayImage("e_Surprise.jpg", 1)
+    misty.PlayAudio("s_PhraseHello.wav", 30)
+    time.sleep(2)
+    misty.Speak("Good morning")
+    misty.MoveHead(0, 0, 0, 80)
+    misty.MoveArms(-85, -85, 80, 80)
+    time.sleep(2)
+    
