9751645e512b78e47674e3722aa62e43002105e4
NoIdeaWhatIamDoing6 <166052555+NoIdeaWhatIamDoing6@users.noreply.github.com>
2024-06-03 15:38:23 -0700

    Update Misty_TOF_REVB.py

--- Python-SDK-main/mistyPy/Misty_TOF_REVB.py
+++ Python-SDK-main/mistyPy/Misty_TOF_REVB.py
@@ -8,11 +8,13 @@ from mistyPy.Robot import Robot
 from mistyPy.Events import Events
 from mistyPy.EventFilters import EventFilters
 
-ROBOT_IP = "192.168.0.100"  # replace with your correct IP
-STOP_DISTANCE = 0.2  # distance [m]] to stop the robot
+ROBOT_IP = "192.168.0.101"  # replace with your correct IP
+STOP_DISTANCE = 0.3  # distance [m]] to stop the robot
 
 misty_robot = Robot(ROBOT_IP)
-
+list1= ["toffc","toffr","toffl"]
+list2= ["toffc","toffr"]
+list3= ["toffc","toffl"]
 
 def stop_robot():
     misty_robot.stop()
@@ -20,20 +22,27 @@ def stop_robot():
 
 #sense "obstacle" and move accordingly
 def move_away_from_obstacle(sensor_id):
-    
+    misty_robot.stop()
     if "toffc" in sensor_id:
         print(f"Moving backward to avoid obstacle detected by {sensor_id}.")
-        misty_robot.drive(linearVelocity=-40, angularVelocity=0)
+        misty_robot.drive_time(linearVelocity=-40, angularVelocity=0,timeMs=2000)
+        #time.sleep(1)
     elif "tofr" in sensor_id:
-        print(f"Moving forward to avoid obstacle detected by {sensor_id}.")
-        misty_robot.drive(40, 0)
+        #print(f"Moving forward to avoid obstacle detected by {sensor_id}.")
+        misty_robot.drive_time(10, 0,2000)
+        # time.sleep(1)
     elif "toffl" in sensor_id:
-        print(f"Moving right to avoid obstacle detected by {sensor_id}.")
-        misty_robot.drive(20, 45)
+        #print(f"Moving right to avoid obstacle detected by {sensor_id}.")
+        # misty_robot.drive_time(-10, 45,2000)
+        misty_robot.drive_track(leftTrackSpeed = -60, rightTrackSpeed = -20)
+        time.sleep(2)
+        misty_robot.stop()
     elif "toffr" in sensor_id:
-        print(f"Moving left to avoid obstacle detected by {sensor_id}.")
-        misty_robot.drive(20, -45)
-        
+        #print(f"Moving left to avoid obstacle detected by {sensor_id}.")
+        # misty_robot.drive_time(-10, -45,2000)
+        misty_robot.drive_track(leftTrackSpeed = -20, rightTrackSpeed = -60)
+        time.sleep(2)
+        misty_robot.stop()     
 
 
 def tof_callback(message):
@@ -41,11 +50,16 @@ def tof_callback(message):
     sensor_id = message["message"]["sensorId"]
     if distance < STOP_DISTANCE:
         print(f"Object detected by {sensor_id} within {STOP_DISTANCE} meters. Stopping robot.")
-        stop_robot()
+        #stop_robot()
         move_away_from_obstacle(sensor_id)
+        time.sleep(3)
+        print(f"Moving forward")
+        misty_robot.drive(linearVelocity=25, angularVelocity=0)
+        
 
 if __name__ == "__main__":
     try:
+
         # Subscribe to the front ToF sensors
         front_right = misty_robot.register_event(
             Events.TimeOfFlight, "frontright", condition=[EventFilters.TimeOfFlightPosition.FrontRight],
@@ -59,20 +73,20 @@ if __name__ == "__main__":
             Events.TimeOfFlight, "frontleft", condition=[EventFilters.TimeOfFlightPosition.FrontLeft], keep_alive=True,
             callback_function=tof_callback, debounce=0
         )
-
-        # Subscribe to the back ToF sensors
-        # back_right = misty_robot.register_event(
-        #     Events.TimeOfFlight, "backright", condition=[EventFilters.TimeOfFlightPosition.BackRight], keep_alive=True,
-        #     callback_function=tof_callback, debounce=0
-        # )
-        back_center = misty_robot.register_event(
+        '''back_center = misty_robot.register_event(
             Events.TimeOfFlight, "backcenter", condition=[EventFilters.TimeOfFlightPosition.Back], keep_alive=True,
             callback_function=tof_callback, debounce=0
-        )
-        # back_left = misty_robot.register_event(
-        #     Events.TimeOfFlight, "backleft", condition=[EventFilters.TimeOfFlightPosition.BackLeft], keep_alive=True,
-        #     callback_function=tof_callback, debounce=0
-        # )  
+        )'''
+        while True:
+            misty_robot.drive_track(leftTrackSpeed = -60, rightTrackSpeed = -20)
+            time.sleep(2)
+            misty_robot.stop()
+            misty_robot.drive_track(leftTrackSpeed = -20, rightTrackSpeed = -60)
+            time.sleep(2)
+            misty_robot.stop()            
+            misty_robot.drive(linearVelocity=25, angularVelocity=0)
+            time.sleep(3)
+        
         #misty.StartObjectDetector()
         #def recognized(data):
         #print(data)
